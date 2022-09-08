f554173a84d62686910dc8c59d57f4660f179cea
Anantha Kumaran <ananthakumaran@gmail.com>
2022-09-08 13:14:13 +0530

    improve init script

--- cmd/init.go
+++ cmd/init.go
@@ -10,16 +10,19 @@ import (
 
 	"strings"
 
+	"math/rand"
+
 	"github.com/ananthakumaran/paisa/internal/model/price"
 	"github.com/ananthakumaran/paisa/internal/scraper/mutualfund"
+	"github.com/ananthakumaran/paisa/internal/scraper/nps"
 	"github.com/ananthakumaran/paisa/internal/utils"
 	"github.com/google/btree"
+	"github.com/samber/lo"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"math/rand"
 )
 
-const START_YEAR = 2015
+const START_YEAR = 2014
 
 var initCmd = &cobra.Command{
 	Use:   "init",
@@ -38,6 +41,16 @@ var initCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(initCmd)
 }
+
+type GeneratorState struct {
+	Balance      float64
+	EPFBalance   float64
+	Ledger       *os.File
+	YearlySalary float64
+	Rent         float64
+}
+
+var pricesTree map[string]*btree.BTree
 
 func generateConfigFile(cwd string) {
 	configFilePath := filepath.Join(cwd, "paisa.yaml")
@@ -63,6 +76,15 @@ commodities:
   - name: ABCBF
     type: mutualfund
     code: 119533
+  - name: NPS_HDFC_E
+    type: nps
+    code: SM008001
+  - name: NPS_HDFC_C
+    type: nps
+    code: SM008002
+  - name: NPS_HDFC_G
+    type: nps
+    code: SM008003
 `
 	log.Info("Generating config file: ", configFilePath)
 	journalFilePath := filepath.Join(cwd, "personal.ledger")
@@ -73,8 +95,40 @@ commodities:
 	}
 }
 
-func loadPrices(schemeCode string, commodityName string, pricesTree map[string]*btree.BTree) {
-	prices, err := mutualfund.GetNav(schemeCode, commodityName)
+func emitTransaction(file *os.File, date time.Time, payee string, from string, to string, amount float64) {
+	_, err := file.WriteString(fmt.Sprintf(`
+%s %s
+    %s                                %s INR
+    %s
+`, date.Format("2006/01/02"), payee, to, formatFloat(amount), from))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func emitCommodityBuy(file *os.File, date time.Time, commodity string, from string, to string, amount float64) {
+	pc := utils.BTreeDescendFirstLessOrEqual(pricesTree[commodity], price.Price{Date: date})
+	_, err := file.WriteString(fmt.Sprintf(`
+%s Investment
+    %s                      %s %s @    %s INR
+    %s
+`, date.Format("2006/01/02"), to, formatFloat(amount/pc.Value), commodity, formatFloat(pc.Value), from))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func loadPrices(schemeCode string, commodityType price.CommodityType, commodityName string, pricesTree map[string]*btree.BTree) {
+	var prices []*price.Price
+	var err error
+
+	switch commodityType {
+	case price.MutualFund:
+		prices, err = mutualfund.GetNav(schemeCode, commodityName)
+	case price.NPS:
+		prices, err = nps.GetNav(schemeCode, commodityName)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -90,8 +144,49 @@ func formatFloat(num float64) string {
 	return strings.TrimRight(strings.TrimRight(s, "0"), ".")
 }
 
-func emitSalary(file *os.File, start time.Time) {
-	var salary float64 = 100000 + (float64(start.Year())-START_YEAR)*(100000*0.05)
+func roundToK(amount float64) float64 {
+	if amount < 20000 {
+		return float64(int(amount/100) * 100)
+	}
+	return float64(int(amount/1000) * 1000)
+}
+
+func incrementByPercentRange(amount float64, min int, max int) float64 {
+	return roundToK(amount + amount*percentRange(min, max))
+}
+
+func percentRange(min int, max int) float64 {
+	if min == max {
+		return float64(min) * 0.01
+	}
+	return float64(randRange(min, max)) * 0.01
+}
+
+func randRange(min int, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func taxRate(amount float64) float64 {
+	if amount < 500000 {
+		return 0
+	} else if amount < 750000 {
+		return 0.10
+	} else if amount < 1000000 {
+		return 0.15
+	} else if amount < 1250000 {
+		return 0.20
+	} else if amount < 1500000 {
+		return 0.25
+	}
+	return 0.30
+}
+
+func emitSalary(state *GeneratorState, start time.Time) {
+	if start.Month() == time.April {
+		state.YearlySalary = incrementByPercentRange(state.YearlySalary, 10, 15)
+	}
+
+	var salary float64 = state.YearlySalary / 12
 	var company string
 	if start.Year() > 2017 {
 		company = "Globex"
@@ -99,73 +194,63 @@ func emitSalary(file *os.File, start time.Time) {
 		company = "Acme"
 	}
 
-	_, err := file.WriteString(fmt.Sprintf(`
-%s Salary
-    Income:Salary:%s
-    Assets:Debt:EPF                 %s INR
-    Expenses:Tax                    %s INR
-    Assets:Checking                 %s INR
-`, start.Format("2006/01/02"), company, formatFloat(salary*0.12), formatFloat(salary*0.20), formatFloat(salary*0.68)))
-	if err != nil {
-		log.Fatal(err)
-	}
+	tax := salary * taxRate(state.YearlySalary)
+	epf := salary * 0.12
+	nps := salary * 0.10
+	state.EPFBalance += epf
+	netSalary := salary - tax - epf - nps
+	state.Balance += netSalary
 
-	if start.Year() > START_YEAR && start.Month() == time.March {
-		_, err = file.WriteString(fmt.Sprintf(`
-%s EPF Interest
-    Income:Interest:EPF
-    Assets:Debt:EPF                 %s INR
-`, start.Format("2006/01/02"), formatFloat(salary*0.12*((float64(start.Year())-START_YEAR)*12)*0.075)))
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-	}
-
+	salaryAccount := fmt.Sprintf("Income:Salary:%s", company)
+	emitTransaction(state.Ledger, start, "Salary", salaryAccount, "Assets:Checking", netSalary)
+	emitTransaction(state.Ledger, start, "Salary EPF", salaryAccount, "Assets:Debt:EPF", epf)
+	emitTransaction(state.Ledger, start, "Salary Tax", salaryAccount, "Expenses:Tax", tax)
+	emitCommodityBuy(state.Ledger, start, "NPS_HDFC_E", salaryAccount, "Assets:Debt:NPS:HDFC:E", nps*0.75)
+	emitCommodityBuy(state.Ledger, start, "NPS_HDFC_C", salaryAccount, "Assets:Equity:NPS:HDFC:C", nps*0.15)
+	emitCommodityBuy(state.Ledger, start, "NPS_HDFC_G", salaryAccount, "Assets:Equity:NPS:HDFC:G", nps*0.10)
 }
 
-func emitEquityMutualFund(file *os.File, start time.Time, pricesTree map[string]*btree.BTree) {
-	multiplier := 1.0
-	if start.Year() > START_YEAR+1 && rand.Intn(3) == 0 {
-		multiplier = -1.0
-	}
-	pc := utils.BTreeDescendFirstLessOrEqual(pricesTree["NIFTY"], price.Price{Date: start})
-	_, err := file.WriteString(fmt.Sprintf(`
-%s Mutual Fund Nifty
-    Assets:Equity:NIFTY  %s NIFTY @ %s INR
-    Assets:Checking
-`, start.Format("2006/01/02"), formatFloat(10000/pc.Value*multiplier), formatFloat(pc.Value)))
-	if err != nil {
-		log.Fatal(err)
+func emitExpense(state *GeneratorState, start time.Time) {
+	if start.Month() == time.April {
+		state.Rent = incrementByPercentRange(state.Rent, 5, 10)
 	}
 
-	pc = utils.BTreeDescendFirstLessOrEqual(pricesTree["NIFTY_JR"], price.Price{Date: start})
-	_, err = file.WriteString(fmt.Sprintf(`
-%s Mutual Fund Nifty Next 50
-    Assets:Equity:NIFTY_JR  %s NIFTY_JR @ %s INR
-    Assets:Checking
-`, start.Format("2006/01/02"), formatFloat(10000/pc.Value*multiplier), formatFloat(pc.Value)))
-	if err != nil {
-		log.Fatal(err)
+	emit := func(payee string, account string, amount float64, fuzz float64) {
+		actualAmount := roundToK(percentRange(int(fuzz*100), 100) * amount)
+		emitTransaction(state.Ledger, start, payee, "Assets:Checking", account, actualAmount)
+		state.Balance -= actualAmount
 	}
 
+	emit("Rent", "Expenses:Rent", state.Rent, 1.0)
+	emit("Internet", "Expenses:Utilities", 1500, 1.0)
+	emit("Mobile", "Expenses:Utilities", 430, 1.0)
+	emit("Shopping", "Expenses:Shopping", 3000, 0.5)
+	emit("Eat out", "Expenses:Restaurants", 2500, 0.5)
+	emit("Groceries", "Expenses:Food", 5000, 0.9)
+
+	if lo.Contains([]time.Month{time.January, time.April, time.November, time.December}, start.Month()) {
+		emit("Dress", "Expenses:Clothing", 5000, 0.5)
+	}
 }
+func emitInvestment(state *GeneratorState, start time.Time) {
+	if start.Month() == time.April {
+		epfInterest := state.EPFBalance * 0.08
+		emitTransaction(state.Ledger, start, "EPF Interest", "Income:Interest:EPF", "Assets:Debt:EPF", epfInterest)
+		state.EPFBalance += epfInterest
+	}
 
-func emitDebtMutualFund(file *os.File, start time.Time, pricesTree map[string]*btree.BTree) {
-	multiplier := 1.0
-	if start.Year() > START_YEAR+1 && rand.Intn(3) == 0 {
-		multiplier = -1.0
-	}
-	pc := utils.BTreeDescendFirstLessOrEqual(pricesTree["ABCBF"], price.Price{Date: start})
-	_, err := file.WriteString(fmt.Sprintf(`
-%s Mutual Fund Birla Corporate Fund
-    Assets:Debt:ABCBF  %s ABCBF @ %s INR
-    Assets:Checking
-`, start.Format("2006/01/02"), formatFloat(10000/pc.Value*multiplier), formatFloat(pc.Value)))
-	if err != nil {
-		log.Fatal(err)
-	}
+	equity1 := roundToK(state.Balance * 0.5)
+	equity2 := roundToK(state.Balance * 0.2)
+	debt := roundToK(state.Balance * 0.3)
+
+	state.Balance -= equity1
+	emitCommodityBuy(state.Ledger, start, "NIFTY", "Assets:Checking", "Assets:Equity:NIFTY", equity1)
+
+	state.Balance -= equity2
+	emitCommodityBuy(state.Ledger, start, "NIFTY_JR", "Assets:Checking", "Assets:Equity:NIFTY_JR", equity2)
+
+	state.Balance -= debt
+	emitCommodityBuy(state.Ledger, start, "ABCBF", "Assets:Checking", "Assets:Debt:ABCBF", debt)
 }
 
 func generateJournalFile(cwd string) {
@@ -182,14 +267,19 @@ func generateJournalFile(cwd string) {
 		log.Fatal(err)
 	}
 
-	pricesTree := make(map[string]*btree.BTree)
-	loadPrices("120716", "NIFTY", pricesTree)
-	loadPrices("120684", "NIFTY_JR", pricesTree)
-	loadPrices("119533", "ABCBF", pricesTree)
+	pricesTree = make(map[string]*btree.BTree)
+	loadPrices("120716", price.MutualFund, "NIFTY", pricesTree)
+	loadPrices("120684", price.MutualFund, "NIFTY_JR", pricesTree)
+	loadPrices("119533", price.MutualFund, "ABCBF", pricesTree)
+	loadPrices("SM008001", price.NPS, "NPS_HDFC_E", pricesTree)
+	loadPrices("SM008002", price.NPS, "NPS_HDFC_C", pricesTree)
+	loadPrices("SM008003", price.NPS, "NPS_HDFC_G", pricesTree)
+
+	state := GeneratorState{Balance: 0, Ledger: ledgerFile, YearlySalary: 500000, Rent: 10000}
 
 	for ; start.Before(end); start = start.AddDate(0, 1, 0) {
-		emitSalary(ledgerFile, start)
-		emitEquityMutualFund(ledgerFile, start, pricesTree)
-		emitDebtMutualFund(ledgerFile, start, pricesTree)
+		emitSalary(&state, start)
+		emitExpense(&state, start)
+		emitInvestment(&state, start)
 	}
 }

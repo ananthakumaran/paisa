165918fda14bb35a776bed9094f4eb700c3a1ccb
Anantha Kumaran <ananthakumaran@gmail.com>
2023-03-26 12:12:58 +0530

    add breadcrumb with links to help page

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,184 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import Sync from "$lib/components/Sync.svelte";
+  import _ from "lodash";
+  export let isBurger: boolean = null;
+
+  interface Link {
+    label: string;
+    href: string;
+    help?: string;
+    children?: Link[];
+  }
+  const links: Link[] = [
+    { label: "Overview", href: "/" },
+    { label: "Expenses", href: "/expense" },
+    {
+      label: "Assets",
+      href: "/assets",
+      children: [
+        { label: "Balance", href: "/balance" },
+        { label: "Investment", href: "/investment" },
+        { label: "Gain", href: "/gain" },
+        { label: "Allocation", href: "/allocation", help: "allocation-targets" },
+        { label: "Stock Portfolio", href: "/portfolio" }
+      ]
+    },
+    {
+      label: "Liabilities",
+      href: "/liabilities",
+      children: [
+        { label: "Balance", href: "/balance" },
+        { label: "Interest", href: "/interest" }
+      ]
+    },
+    { label: "Income", href: "/income" },
+    {
+      label: "Ledger",
+      href: "/ledger",
+      children: [
+        { label: "Transactions", href: "/transaction" },
+        { label: "Journal", href: "/journal" },
+        { label: "Price", href: "/price" }
+      ]
+    },
+    {
+      label: "Tax",
+      href: "/tax",
+      help: "tax",
+      children: [
+        { label: "Harvest", href: "/harvest", help: "tax-harvesting" },
+        { label: "Capital Gains", href: "/capital_gains", help: "capital-gains" },
+        { label: "Schedule AL", href: "/schedule_al", help: "schedule-al" }
+      ]
+    },
+    { label: "Retirement", href: "/retirement/progress", help: "retirement" },
+    { label: "Doctor", href: "/doctor" }
+  ];
+
+  let selectedLink: Link = null;
+  let selectedSubLink: Link = null;
+
+  $: if ($page.url.pathname) {
+    selectedSubLink = null;
+    selectedLink = _.find(links, (l) => $page.url.pathname == l.href);
+    if (!selectedLink) {
+      selectedLink = _.find(
+        links,
+        (l) => !_.isEmpty(l.children) && $page.url.pathname.startsWith(l.href)
+      );
+
+      selectedSubLink = _.find(
+        selectedLink.children,
+        (l) => $page.url.pathname == selectedLink.href + l.href
+      );
+    }
+  }
+</script>
+
+<nav class="navbar is-transparent" aria-label="main navigation">
+  <div class="navbar-brand">
+    <span class="navbar-item is-size-4 has-text-weight-medium">₹ Paisa</span>
+    <a
+      role="button"
+      class="navbar-burger"
+      class:is-active={isBurger === true}
+      on:click|preventDefault={(_e) => (isBurger = !isBurger)}
+      aria-label="menu"
+      aria-expanded="false"
+      data-target="navbarBasicExample"
+    >
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+    </a>
+  </div>
+
+  <div class="navbar-menu" class:is-active={isBurger === true}>
+    <div class="navbar-start">
+      {#each links as link}
+        {#if _.isEmpty(link.children)}
+          <a class="navbar-item" href={link.href} class:is-active={$page.url.pathname == link.href}
+            >{link.label}</a
+          >
+        {:else}
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link" class:is-active={$page.url.pathname.startsWith(link.href)}
+              >{link.label}</a
+            >
+            <div class="navbar-dropdown is-boxed">
+              {#each link.children as sublink}
+                {@const href = link.href + sublink.href}
+                <a class="navbar-item" {href} class:is-active={$page.url.pathname === href}
+                  >{sublink.label}</a
+                >
+              {/each}
+            </div>
+          </div>
+        {/if}
+      {/each}
+    </div>
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="field is-grouped">
+          <p class="control">
+            <Sync />
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+
+{#if selectedLink}
+  <nav
+    style="margin-left: 12px;"
+    class="breadcrumb has-chevron-separator mb-0 is-small"
+    aria-label="breadcrumbs"
+  >
+    <ul>
+      <li>
+        <a class="is-inactive">{selectedLink.label}</a>
+        {#if selectedLink.help}
+          <a
+            style="margin-left: -10px;"
+            class="p-0"
+            href={`https://ananthakumaran.in/paisa/${selectedLink.help}.html`}
+            ><span class="icon is-small">
+              <i class="fas fa-question fa-border" />
+            </span></a
+          >
+        {/if}
+      </li>
+      {#if selectedSubLink}
+        <li>
+          <a class="is-inactive">{selectedSubLink.label}</a>
+
+          {#if selectedSubLink.help}
+            <a
+              style="margin-left: -10px;"
+              class="p-0"
+              href={`https://ananthakumaran.in/paisa/${selectedSubLink.help}.html`}
+              ><span class="icon is-small">
+                <i class="fas fa-question fa-border" />
+              </span></a
+            >
+          {/if}
+        </li>
+      {/if}
+    </ul>
+  </nav>
+{/if}
+
+<style lang="scss">
+  .breadcrumb.has-chevron-separator li + li::before {
+    content: "";
+    color: hsl(229deg, 53%, 53%);
+  }
+
+  a.is-inactive {
+    color: hsl(0deg, 0%, 21%);
+    cursor: default;
+    pointer-events: none;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-  import { page } from "$app/stores";
   import { afterNavigate, beforeNavigate } from "$app/navigation";
   import { followCursor, type Instance, delegate } from "tippy.js";
   import _ from "lodash";
-  import Sync from "$lib/components/Sync.svelte";
   import Spinner from "$lib/components/Spinner.svelte";
+  import Navbar from "$lib/components/Navbar.svelte";
 
   let isBurger: boolean = null;
   let tippyInstances: Instance[] = [];
@@ -35,166 +34,7 @@
   });
 </script>
 
-<nav class="navbar is-transparent" aria-label="main navigation">
-  <div class="navbar-brand">
-    <span class="navbar-item is-size-4 has-text-weight-medium">₹ Paisa</span>
-    <a
-      role="button"
-      class="navbar-burger"
-      class:is-active={isBurger === true}
-      on:click|preventDefault={(_e) => (isBurger = !isBurger)}
-      aria-label="menu"
-      aria-expanded="false"
-      data-target="navbarBasicExample"
-    >
-      <span aria-hidden="true" />
-      <span aria-hidden="true" />
-      <span aria-hidden="true" />
-    </a>
-  </div>
-
-  <div class="navbar-menu" class:is-active={isBurger === true}>
-    <div class="navbar-start">
-      <a id="overview" class="navbar-item" href="/" class:is-active={$page.url.pathname === "/"}
-        >Overview</a
-      >
-      <a
-        id="expense"
-        class="navbar-item"
-        href="/expense"
-        class:is-active={$page.url.pathname === "/expense"}>Expense</a
-      >
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link" class:is-active={$page.url.pathname.startsWith("/assets/")}>Assets</a
-        >
-        <div class="navbar-dropdown is-boxed">
-          <a
-            id="asset-balance"
-            class="navbar-item"
-            href="/assets/balance"
-            class:is-active={$page.url.pathname === "/assets/balance"}>Balance</a
-          >
-          <a
-            id="investment"
-            class="navbar-item"
-            href="/assets/investment"
-            class:is-active={$page.url.pathname === "/assets/investment"}>Investment</a
-          >
-          <a
-            id="gain"
-            class="navbar-item"
-            href="/assets/gain"
-            class:is-active={$page.url.pathname === "/assets/gain"}>Gain</a
-          >
-          <a
-            id="allocation"
-            class="navbar-item"
-            href="/assets/allocation"
-            class:is-active={$page.url.pathname === "/assets/allocation"}>Allocation</a
-          >
-          <a
-            id="portfolio"
-            class="navbar-item"
-            href="/assets/portfolio"
-            class:is-active={$page.url.pathname === "/assets/portfolio"}>Stock Portfolio</a
-          >
-        </div>
-      </div>
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link" class:is-active={$page.url.pathname.startsWith("/liabilities/")}
-          >Liabilities</a
-        >
-        <div class="navbar-dropdown is-boxed">
-          <a
-            class="navbar-item"
-            href="/liabilities/balance"
-            class:is-active={$page.url.pathname === "/liabilities/balance"}>Balance</a
-          >
-
-          <a
-            class="navbar-item"
-            href="/liabilities/interest"
-            class:is-active={$page.url.pathname === "/liabilities/interest"}>Interest</a
-          >
-        </div>
-      </div>
-      <a
-        id="income"
-        class="navbar-item"
-        href="/income"
-        class:is-active={$page.url.pathname === "/income"}>Income</a
-      >
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link" class:is-active={$page.url.pathname.startsWith("/ledger/")}>Ledger</a
-        >
-        <div class="navbar-dropdown is-boxed">
-          <a
-            id="transaction"
-            class="navbar-item"
-            href="/ledger/transaction"
-            class:is-active={$page.url.pathname === "/ledger/transaction"}>Transactions</a
-          >
-          <a
-            id="journal"
-            class="navbar-item"
-            href="/ledger/journal"
-            class:is-active={$page.url.pathname === "/ledger/journal"}>Journal</a
-          >
-          <a
-            id="price"
-            class="navbar-item"
-            href="/ledger/price"
-            class:is-active={$page.url.pathname === "/ledger/price"}>Price</a
-          >
-        </div>
-      </div>
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link" class:is-active={$page.url.pathname.startsWith("/tax/")}>Tax</a>
-        <div class="navbar-dropdown is-boxed">
-          <a
-            id="harvest"
-            class="navbar-item"
-            href="/tax/harvest"
-            class:is-active={$page.url.pathname === "/tax/harvest"}>Harvest</a
-          >
-          <a
-            id="capital_gains"
-            class="navbar-item"
-            href="/tax/capital_gains"
-            class:is-active={$page.url.pathname === "/tax/capital_gains"}>Capital Gains</a
-          >
-          <a
-            id="schedule_al"
-            class="navbar-item"
-            href="/tax/schedule_al"
-            class:is-active={$page.url.pathname === "/tax/schedule_al"}>Schedule AL</a
-          >
-        </div>
-      </div>
-      <a
-        id="retirement_progress"
-        class="navbar-item"
-        href="/retirement/progress"
-        class:is-active={$page.url.pathname === "/retirement/progress"}>Retirement</a
-      >
-      <a
-        id="doctor"
-        class="navbar-item"
-        href="/doctor"
-        class:is-active={$page.url.pathname === "/doctor"}>Doctor</a
-      >
-    </div>
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="field is-grouped">
-          <p class="control">
-            <Sync />
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-</nav>
+<Navbar {isBurger} />
 
 <Spinner>
   <slot />

162250d60b3c9668a6409125c7ce344e32319ffa
ananthakumaran <ananthakumaran@gmail.com>
2023-09-16 02:35:31 +0000

    Deploying to gh-pages from @ ananthakumaran/paisa@c2e4410a66fb88edea79622d166e27f048af46f1 🚀

---
+++ lexer/ledger.py
@@ -0,0 +1,29 @@
+from pygments.lexer import RegexLexer, bygroups
+from pygments.token import *
+
+__all__ = ['LedgerLexer']
+
+class LedgerLexer(RegexLexer):
+     name = "ledger"
+     aliases = ["ledger"]
+     filenames = ['*.ledger', '*.journal']
+     mimetypes = ['text/x-ledger']
+
+     tokens = {
+          'root': [
+               (r'=.*$', Text),
+               (r'^(\d{4}/\d{2}/\d{2})(/[*][^*]+[*]/)(\s*)(.+)(/[*][^*]+[*]/)$',
+                bygroups(Name.Attribute, Comment.Multiline, Text, Generic.Heading, Comment.Multiline)),
+               (r';(.*?)$', Comment.Single),
+               (r'/(\\\n)?[*](.|\n)*?[*](\\\n)?/', Comment.Multiline),
+               (r'^(\d{4}/\d{2}/\d{2})(\s*)(.+)$',
+                bygroups(Name.Attribute, Text, Generic.Heading)),
+               (r'\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}', Name.Attribute),
+               (r'\d{4}/\d{2}/\d{2}', Name.Attribute),
+               (r'[@]', Operator),
+               (r'[+-]?(?:[0-9,])+(\.(?:[0-9,])+)?', Number),
+               (r'[$€]', Keyword),
+               (r'\b[A-Z_]+\b', Keyword),
+               (r'^\s{4}[^\][(); \t\n]((?!\s{2})[^/\][();\t\n])*', String),
+          ]
+     }

24b9fee595f88216682a36ab67ffd33f2a83eea6
Anantha Kumaran <ananthakumaran@gmail.com>
2023-11-02 18:52:17 +0530

    dry logo svg

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
-  import COLORS from "$lib/colors";
   export let size = 256;
+  export let animation = false;
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24"
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width={size}
+  height={size}
+  viewBox="0 0 24 24"
+  class:animation
   ><g transform="rotate(90 12 12)">
     <path
       fill="currentColor"
       d="M 12 5 C 7.031 5 2 6.546 2 9.5 S 7.031 14 12 14 C 16.97 14 22 12.454 22 9.5 S 16.97 5 12 5 Z"
     />
+
     <path
       fill="currentColor"
       d="M 7 14.938 V 17.938 C 8.237 18.237 9.605 18.42 11 18.479 V 15.479 A 21.166 21.166 0 0 1 7 14.938 Z"
@@ -27,5 +33,30 @@
       fill="currentColor"
       d="M 5 17.297 V 14.297 C 3.2 13.542 2 12.439 2 11 V 14 C 2 15.439 3.2 16.542 5 17.297 Z"
     />
-  </g></svg
->
+  </g>
+
+  {#if animation}
+    <g
+      transform="translate(2.6,5.5),scale(0.8),rotate(120,12,12),matrix(0.16741001,-0.09665704,0.04832852,0.083705,7.3774358,11.493257)"
+    >
+      <path d="M 0,70 A 65,70 0 0 0 65,0 5,5 0 0 1 75,0 75,70 0 0 1 0,70 Z">
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          from="360 0 0"
+          to="0 0 0"
+          dur="0.6s"
+          repeatCount="indefinite"
+        />
+      </path>
+    </g>
+  {/if}
+</svg>
+
+<style lang="scss">
+  @import "bulma/sass/utilities/_all.sass";
+
+  svg.animation {
+    filter: drop-shadow(-1px -1px 1px rgba($black, 0.4));
+  }
+</style>
--- src/lib/components/Spinner.svelte
+++ src/lib/components/Spinner.svelte
@@ -1,47 +1,13 @@
 <script lang="ts">
   import { delayedLoading, loading } from "../../store";
+  import Logo from "./Logo.svelte";
   let size = 48;
 </script>
 
 <div>
   {#if $delayedLoading}
     <div class="circle-container">
-      <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24">
-        <g transform="rotate(90 12 12)">
-          <path
-            d="M 12 5 C 7.031 5 2 6.546 2 9.5 S 7.031 14 12 14 C 16.97 14 22 12.454 22 9.5 S 16.97 5 12 5 Z"
-          />
-          <path
-            d="M 7 14.938 V 17.938 C 8.237 18.237 9.605 18.42 11 18.479 V 15.479 A 21.166 21.166 0 0 1 7 14.938 Z"
-          />
-          <path
-            d="M 13 15.478 V 18.478 A 20.994 20.994 0 0 0 17 17.937 V 14.937 A 20.994 20.994 0 0 1 13 15.478 Z"
-          />
-
-          <path
-            d="M 19 14.297 V 17.297 C 20.801 16.542 22 15.44 22 14 V 11 C 22 12.44 20.801 13.542 19 14.297 Z"
-          />
-
-          <path
-            d="M 5 17.297 V 14.297 C 3.2 13.542 2 12.439 2 11 V 14 C 2 15.439 3.2 16.542 5 17.297 Z"
-          />
-        </g>
-
-        <g
-          transform="translate(2.6,5.5),scale(0.8),rotate(120,12,12),matrix(0.16741001,-0.09665704,0.04832852,0.083705,7.3774358,11.493257)"
-        >
-          <path d="M 0,70 A 65,70 0 0 0 65,0 5,5 0 0 1 75,0 75,70 0 0 1 0,70 Z">
-            <animateTransform
-              attributeName="transform"
-              type="rotate"
-              from="360 0 0"
-              to="0 0 0"
-              dur="0.6s"
-              repeatCount="indefinite"
-            />
-          </path>
-        </g>
-      </svg>
+      <Logo {size} animation />
     </div>
   {/if}
   <div style={$loading ? "display: none" : ""}>

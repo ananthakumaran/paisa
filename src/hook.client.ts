e8ca874b5ba7d83138bfa717a0ffa03b38f07b78
Anantha Kumaran <ananthakumaran@gmail.com>
2023-12-14 22:14:40 +0530

    remove dummy error pages

--- src/hook.client.ts
+++
@@ -1,6 +0,0 @@
-import type { HandleClientError } from "@sveltejs/kit";
-
-export const handleError: HandleClientError = async ({ error, event }) => {
-  console.error("handle error", error, event);
-  return { message: "Whoops!" };
-};
--- src/routes/(app)/+error.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  import { page } from "$app/stores";
-</script>
-
-<h1>{$page.error.message}</h1>

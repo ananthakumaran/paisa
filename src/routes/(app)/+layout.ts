144f747c3be2145b3154edfe0f1e81aa4650819f
Anantha Kumaran <ananthakumaran@gmail.com>
2023-12-18 21:15:19 +0530

    don't catch all errors
    Only redirect to login page on 401

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -1,20 +1,12 @@
 import type { LayoutLoad } from "./$types";
-
 import { ajax, configUpdated, setNow } from "$lib/utils";
 
-import { goto } from "$app/navigation";
-
-export const load = (async ({}) => {
-  try {
-    const { config, now } = await ajax("/api/config");
-    if (now) {
-      setNow(now);
-    }
-    globalThis.USER_CONFIG = config;
-    configUpdated();
-    return {};
-  } catch (e) {
-    globalThis.USER_CONFIG = {} as any;
-    await goto("/login");
+export const load = (async () => {
+  const { config, now } = await ajax("/api/config");
+  if (now) {
+    setNow(now);
   }
+  globalThis.USER_CONFIG = config;
+  configUpdated();
+  return {};
 }) satisfies LayoutLoad;

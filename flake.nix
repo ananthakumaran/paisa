a53a60467a576b4be4102bb6145bf0a36420edfc
Anantha Kumaran <ananthakumaran@gmail.com>
2025-05-10 08:25:59 +0530

    Pin hledger version

--- flake.nix
+++ flake.nix
@@ -1,19 +1,22 @@
 {
   description = "paisa";
-  inputs.mkdocs-pkgs.url = "github:NixOS/nixpkgs/staging-next";
+  # Pin hldeger to 1.32.x; 1.40 has a bug related to chained price calculation
+  # https://github.com/simonmichael/hledger/issues/2254
+  inputs.hledger-pkgs.url =
+    "github:NixOS/nixpkgs/ebe4301cbd8f81c4f8d3244b3632338bbeb6d49c";
 
-  outputs = { self, nixpkgs, flake-utils, mkdocs-pkgs }:
+  outputs = { self, nixpkgs, flake-utils, hledger-pkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        mkdocs = mkdocs-pkgs.legacyPackages.${system};
+        hledger = hledger-pkgs.legacyPackages.${system};
         nodeDependencies = (pkgs.callPackage ./flake/override.nix {
           nodejs = pkgs.nodejs_22;
         }).nodeDependencies;
       in {
         devShells.default = import ./shell.nix {
           inherit pkgs;
-          inherit mkdocs;
+          inherit hledger;
         };
 
         packages.default = pkgs.buildGoModule {
